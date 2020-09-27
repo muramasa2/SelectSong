a260c6ab7658f260e6ce886a0727062ea573cad4
muramasa2 <masasoundmusic@gmail.com>
2020-09-27 23:05:50 +0900

    add swich

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def handle_message(event):
     if event.message.text == '次の曲を教えて':
         return_text = song_list[np.random.randint(0, len(song_list))]
     else:
-        return_text = '無効な入力です。\n「次の曲を教えて」と入力して見てね！'
+        return_text = '無効な入力です。\n「次の曲を教えて」と入力してみてね！'
     
     line_bot_api.reply_message(
     event.reply_token,
